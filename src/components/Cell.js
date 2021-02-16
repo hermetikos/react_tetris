fe5fa8e566ce442ffc14107d26f90d6554be08a6
Stan S <sslup@hermetikos.com>
2021-02-16 01:08:38 -0500

    Adds style to stage

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -5,7 +5,7 @@ import {TETROMINOS} from "../tetrominos";
 import {StyledCell} from './styles/StyledCell';
 
 const Cell = ({ type }) => (
-    <StyledCell type={'L'} color={TETROMINOS['L'].color}>cell</StyledCell>
+    <StyledCell type={'L'} color={TETROMINOS['L'].color} />
 )
 
 export default Cell;
